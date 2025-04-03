f30068a9189b4c0dd186510a95e65ca4a04e824f
PinQusestion <mananbansal238@gmail.com>
2025-04-03 12:48:02 +0530

    fixed a problem

--- src/App.jsx
+++ src/App.jsx
@@ -47,9 +47,9 @@ function App() {
           />
           <button
             onClick={copyPasswordToClipboard}
-            className="outline-none bg-blue-700 text-white px-3 py-2 shrink-0 cursor-pointer hover:bg-black text-white"
+            className="outline-none bg-blue-700 text-white. px-3 py-2 shrink-0 cursor-pointer hover:bg-black text-white"
           >
-            copy
+            Copy
           </button>
         </div>
         <div className="flex text-sm gap-x-2">
